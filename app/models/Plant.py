bc7c62e973191e2117549d7546576e99fb9da158
Gerhard van Zyl <gerhard@schoongezicht.com>
2022-10-18 20:55:50 +0200

    Updated DB and Plant.py with plant data with scientific name

--- app/models/Plant.py
+++ app/models/Plant.py
@@ -7,6 +7,21 @@ class Plant(db.Model):
     __tablename__ = "plant"
     id = Column(Integer, primary_key=True)
     name = Column(String)
+    scientific_name = Column(String)
+    category = Column(String)
+    max_light_mmol = Column(Integer)
+    min_light_mmol = Column(Integer)
+    max_light_lux = Column(Integer)
+    min_light_lux = Column(Integer)
+    max_temp = Column(Integer)
+    min_temp = Column(Integer)
+    max_env_humid = Column(Integer)
+    min_env_humid = Column(Integer)
+    max_soil_moist = Column(Integer)
+    min_soil_moist = Column(Integer)
+    max_soil_ec = Column(Integer)
+    min_soil_ec = Column(Integer)
+    url = Column(String)
     # condition = relationship("Condition")
 
 
